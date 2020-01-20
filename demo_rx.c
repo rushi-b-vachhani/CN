daa2d99929434342854ab5ef96b29b7d6e57414f
rushi-b-vachhani <52479068+rushi-b-vachhani@users.noreply.github.com>
2020-01-20 12:49:30 +0530

    Add files via upload

--- demo_rx.c
+++ demo_rx.c
@@ -12,7 +12,7 @@ compile with the command: gcc demo_rx.c rs232.c -Wall -Wextra -o2 -o test_rx
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "checksum_algo.h"
 #ifdef _WIN32
 #include <Windows.h>
 #else
@@ -21,52 +21,53 @@ compile with the command: gcc demo_rx.c rs232.c -Wall -Wextra -o2 -o test_rx
 
 #include "rs232.h"
 
-
-
 int main()
 {
-  int i, n,
-      cport_nr=0,        /* /dev/ttyS0 (COM1 on windows) */
-      bdrate=9600;       /* 9600 baud */
+	int i, n,
+		cport_nr = 0,  /* /dev/ttyS0 (COM1 on windows) */
+		bdrate = 9600; /* 9600 baud */
 
-  unsigned char buf[4096];
+	unsigned char buf[4096];
+	int check;
+	char mode[] = {'8', 'N', '1', 0};
+	char str2[512];
 
-  char mode[]={'8','N','1',0};
+	if (RS232_OpenComport(cport_nr, bdrate, mode))
+	{
+		printf("Can not open comport\n");
 
+		return (0);
+	}
 
-  if(RS232_OpenComport(cport_nr, bdrate, mode))
-  {
-    printf("Can not open comport\n");
+	while (1)
+	{
+		n = RS232_PollComport(cport_nr, buf, 4095);
 
-    return(0);
-  }
+		if (n > 0)
+		{
+			buf[n] = 0; /* always put a "null" at the end of a string! */
 
-  while(1)
-  {
-    n = RS232_PollComport(cport_nr, buf, 4095);
-
-    if(n > 0)
-    {
-      buf[n] = 0;   /* always put a "null" at the end of a string! */
-
-      for(i=0; i < n; i++)
-      {
-        if(buf[i] < 32)  /* replace unreadable control-codes by dots */
-        {
-          buf[i] = '.';
-        }
-      }
-
-      printf("received %i bytes: %s\n", n, (char *)buf);
-    }
+			strcpy(str2, buf);
+			check = check_xor(str2);
+			if (check != 0)
+			{
+				RS232_cputs(cport_nr, "t");
+			}
+			else
+			{
+				RS232_cputs(cport_nr, "f");
+				printf("***Failed***");
+			}
+			
+			printf("received %i bytes: %s\n", n, (char *)buf);
+		}
 
 #ifdef _WIN32
-    Sleep(100);
+		Sleep(100);
 #else
-    usleep(100000);  /* sleep for 100 milliSeconds */
+		usleep(100000); /* sleep for 100 milliSeconds */
 #endif
-  }
+	}
 
-  return(0);
+	return (0);
 }
-
